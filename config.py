0ee5c2f49915c8112bcffdc2019e93bd8415d208
akmoviebot <mrak7753@gmail.com>
2025-02-12 19:14:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "Shortxlinks.com")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "26c58ac6c7879cf25966a3f6fc999c9c1e3c9315")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
-IS_VERIFY = os.environ.get("IS_VERIFY", "True")
+IS_VERIFY = os.environ.get("IS_VERIFY", "False")
 TUT_VID = os.environ.get("TUT_VID", "https://t.me/akmoviefile0/18") # shortxlink ka tut_vid he 
 
 #force sub channel id, if you want enable force sub
